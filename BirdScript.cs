dccacf4333a270c35d1b7509dd4409ff5fd377ad
Krish Mukesh Kumar <142864906+KrishMukesh@users.noreply.github.com>
2024-01-15 16:48:27 +0530

    Add files via upload
    The code for Twiity along with its asset and the logic used within the game.

---
+++ BirdScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "Bob the Bird";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) == true || (Input.GetKeyDown(KeyCode.Mouse0) == true) && birdIsAlive==true)
+        {
+            myRigidBody.velocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        birdIsAlive=false;
+        logic.gameOver();
+    }
+}
